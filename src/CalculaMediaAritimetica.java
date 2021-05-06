31e4da41f792fbac6ef92167271145b61277dc42
Entra21 <entra21@proway.treina>
2021-05-05 21:27:13 -0300

    sistema de calculo de media

---
+++ src/CalculaMediaAritimetica.java
@@ -0,0 +1,16 @@
+/*
+Suponha um programa que solicita ao usuario 2 numeros
+e depois mostra a media dos valores.
+ */
+
+public class CalculaMediaAritimetica {
+    public static void main (String[] args){
+        double primeiroNumero = 3;
+        double segundoNumero = 6;
+        double resultado;
+
+        resultado = (primeiroNumero + segundoNumero)/2;
+
+        System.out.println(resultado);
+    }
+}
