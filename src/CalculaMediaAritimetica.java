a2434ed484bb2ee9c44211235d4ca0c89cefb4e0
Entra21 <entra21@proway.treina>
2021-05-06 19:58:20 -0300

    inclusao do metodo Scanner

--- src/CalculaMediaAritimetica.java
+++ src/CalculaMediaAritimetica.java
@@ -3,13 +3,18 @@ Suponha um programa que solicita ao usuario 2 numeros
 e depois mostra a media dos valores.
  */
 
+import java.util.Scanner;
+
 public class CalculaMediaAritimetica {
     public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Digite o primeiro valor");
-        double primeiroNumero = 3;
+        double primeiroNumero = scanner.nextDouble();
 
         System.out.println("Digite o segundo valor");
-        double segundoNumero = 6;
+        double segundoNumero = scanner.nextDouble();;
 
         double resultado;
         resultado = (primeiroNumero + segundoNumero)/2;
