bdb65150ed3ca7635ceff29e6089cc53c2f39d12
Entra21 <entra21@proway.treina>
2021-05-11 20:34:23 -0300

    exercicio de concatenacao

---
+++ src/Concatenacao.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Concatenacao {
+
+        public static void main(String[] args) {
+
+            Locale.setDefault(Locale.US);
+
+            String nome = "Rafael";
+            int idade  = 20;
+            double altura  = 1.70;
+
+            System.out.printf("O meu nome é %s e tenho %d anos, minha altura é %.2f%n", nome, idade, altura);
+
+
+
+        }
+    }
+
+
+
+
--- src/Formatacao.java
+++ src/Formatacao.java
@@ -3,7 +3,7 @@ import java.util.Locale;
 public class Formatacao {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        double x = 10.35784;
+        double x = 10.35884;
 
         // Imprimi valor com formatação
         System.out.printf("%.2f%n", x);
